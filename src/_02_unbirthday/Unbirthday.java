685aebbd6f287578b2987d5233b044b039690461
League Student <league@iMac.attlocal.net>
2019-09-06 17:00:32 -0700

    Session 1.1

---
+++ src/_02_unbirthday/Unbirthday.java
@@ -0,0 +1,11 @@
+package _02_unbirthday;
+import javax.swing.JOptionPane;
+public class Unbirthday {
+	public static void main(String[] args) {
+		
+		String bday = JOptionPane.showInputDialog("what is your birthday (mm/dd)");
+		if (bday = "9/6") {
+			JOptionPane.showMessageDialog(null, "happy birthday");
+		}
+	}
+}
